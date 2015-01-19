1fc3803290a001c5fcef6e0b064d6886e8dde206
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-01-20 00:44:23 +0300

    + experiments

--- main.go
+++ main.go
@@ -1,12 +1,56 @@
 package main
 
 import (
-	"github.com/google/go-github/github"
 	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/gorilla/mux"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func main()  {
+var (
+	Error *log.Logger
+)
 
+func main() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	filename := filepath.Join(dir, "/gohub.log")
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f.Truncate(0)
+	if err != nil {
+		fmt.Print("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	loggerInit(f)
+	Error.Println("Hello")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", defaultHandler)
+
+	http.Handle("/", router)
+	err2 := http.ListenAndServe(":8080", nil)
+	if err2 != nil {
+		Error.Println("Error on starting server: %v", err2)
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello world")
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		Error.Println("error reading body")
+	}
+	Error.Println(string(body))
+}
+
+func loggerInit(f *os.File) {
+	Error = log.New(f, "", log.Lshortfile)
+	Error.Println("Logger start")
 }
 
 func test() string {
--- main_test.go
+++ main_test.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
-	"github.com/google/go-github/github"
+	"code.google.com/p/goauth2/oauth"
 	"encoding/json"
 	"fmt"
-	"code.google.com/p/goauth2/oauth"
+	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestTest(t *testing.T){
-	assert.Equal(t, test() , "hello world")
+func TestTest(t *testing.T) {
+	assert.Equal(t, test(), "hello world")
 }
 
-func TestConnect(t *testing.T){
+func TestConnect(t *testing.T) {
 	hook := new(github.Hook)
-	json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "https://godoc.org/golang.org/x/oauth2"}}`), &hook)
-//	fmt.Print(hook)
+	json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "http://requestb.in/1c8ldr11"}}`), &hook)
+	//	fmt.Print(hook)
 
-	token := "" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
+	token := "389924dc1c4981bdd9ffce7bb6de96f7ce18faef" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
 	transport := &oauth.Transport{
 		Token: &oauth.Token{AccessToken: token},
 	}
 	client := github.NewClient(transport.Client())
 	fmt.Print(client.Repositories.CreateHook("alexkomrakov", "gohub", hook))
 
-//	options :=& github.ListOptions{1,5}
-//	res, _, _ := client.Repositories.ListHooks("alexkomrakov", "gohub", options)
-//	fmt.Println(res)
+	//	options :=& github.ListOptions{1,5}
+	//	res, _, _ := client.Repositories.ListHooks("alexkomrakov", "gohub", options)
+	//	fmt.Println(res)
 }
-
-

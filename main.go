517ec9f8f99a2534c096acd4f6e6565b7319bad3
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-01-21 23:17:18 +0300

    + executing commands + parsing yaml configs + getting config from repo

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"code.google.com/p/goauth2/oauth"
 	"encoding/json"
 	"fmt"
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/ssh"
+	"gopkg.in/yaml.v2"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,7 +18,7 @@ import (
 )
 
 const (
-// https://gist.github.com/AlexKomrakov/3c3a7bee69da1fb2a328
+	// https://gist.github.com/AlexKomrakov/412a549b693c5f0a03d6
 )
 
 var (
@@ -47,18 +50,51 @@ func githubHandler(w http.ResponseWriter, req *http.Request) {
 
 	listOptions := new(github.ListOptions)
 	json.Unmarshal([]byte(`{"page": 0, "perPage": 5}`), &listOptions)
-	z, c, _ := client.Repositories.ListStatuses(github_user, github_repo, github_token, listOptions)
+	z, c, _ := client.Repositories.ListStatuses(github_user, github_repo, github_ref, listOptions)
 
 	io.WriteString(w, fmt.Sprintf("%v", z))
 	io.WriteString(w, fmt.Sprintf("%v", c))
 
-	setGitStatus(client)
+	setGitStatus(client, "success")
 }
 
-func setGitStatus(client *github.Client) {
-	status := new(github.RepoStatus)
-	json.Unmarshal([]byte(`{"state": "success", "context": "continuous-integration/travis-ci"}`), &status)
-	client.Repositories.CreateStatus(github_user, github_repo, github_token, status)
+func setGitStatus(client *github.Client, state string) {
+	context := "continuous-integration/gorgon-ci"
+	status := &github.RepoStatus{State: &state, Context: &context}
+	client.Repositories.CreateStatus(github_user, github_repo, github_ref, status)
+}
+
+func getGithubFileContent(client *github.Client, owner, repo, filename, branch string) ([]byte, error) {
+	repoOptions := &github.RepositoryContentGetOptions{branch}
+	a, _, _, err1 := client.Repositories.GetContents(owner, repo, filename, repoOptions)
+	if err1 != nil {
+		Error.Println("Error on getting file from github: %v", err1)
+		return nil, err1
+	}
+
+	fileContent, err2 := a.Decode()
+	if err2 != nil {
+		Error.Println("Error on decoding file from github: %v", err2)
+		return nil, err2
+	}
+
+	return fileContent, nil
+}
+
+type ymlConfig struct {
+	Host     []interface{}
+	Commands []interface{}
+}
+
+func readYamlConfig(file []byte) (ymlConfig, error) {
+	config := ymlConfig{}
+	err := yaml.Unmarshal(file, &config)
+	if err != nil {
+		Error.Println("Error on reading yaml config")
+		return config, err
+	}
+
+	return config, nil
 }
 
 func defaultHandler(w http.ResponseWriter, req *http.Request) {
@@ -91,4 +127,23 @@ func loggerInit(filename string) (file *os.File) {
 	Error.Println("Logger start")
 
 	return f
+}
+
+func execSshCommand(client *ssh.Client, command string) (bytes.Buffer, bytes.Buffer) {
+	session, err1 := client.NewSession()
+	if err1 != nil {
+		panic("Failed to create session: " + err1.Error())
+	}
+	defer session.Close()
+
+	var outBuf bytes.Buffer
+	var errBuf bytes.Buffer
+	session.Stdout = &outBuf
+	session.Stderr = &errBuf
+	session.Run(command) //TODO Check error
+	//	if err2 != nil {
+	//		panic("Failed to run: " + err2.Error())
+	//	}
+
+	return outBuf, errBuf
 }
--- main_test.go
+++ main_test.go
@@ -2,35 +2,69 @@ package main
 
 import (
 	"code.google.com/p/goauth2/oauth"
-	"encoding/json"
 	"fmt"
 	"github.com/google/go-github/github"
-	"github.com/stretchr/testify/assert"
+	//	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/ssh"
 	"testing"
 )
 
 func TestConnect(t *testing.T) {
-	hook := new(github.Hook)
-	json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "http://requestb.in/1c8ldr11"}}`), &new(github.Hook))
-	//	fmt.Print(hook)
+	//	hook := new(github.Hook)
+	//	json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "http://requestb.in/1c8ldr11"}}`), &new(github.Hook))
+	//	//	fmt.Print(hook)
+	//
+	//	token := "" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
+	//	transport := &oauth.Transport{
+	//		Token: &oauth.Token{AccessToken: token},
+	//	}
+	//	client := github.NewClient(transport.Client())
+	//	fmt.Print(client.Repositories.CreateHook("alexkomrakov", "gohub", hook))
+	//
+	//	status := new(github.RepoStatus)
+	//	json.Unmarshal([]byte(`{"state": "success"}`), &status)
+	//	_, b, _ := client.Repositories.CreateStatus("alexkomrakov", "gohub", "", status)
+	//	fmt.Println(b)
+	//
+	//	listOptions := new(github.ListOptions)
+	//	json.Unmarshal([]byte(`{"page": 0, "perPage": 5}`), &listOptions)
+	//	z, c, _ := client.Repositories.ListStatuses("alexkomrakov", "gohub", "", listOptions)
+	//	fmt.Println(z)
+	//	fmt.Println(c)
+}
 
-	token := "" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
+func TestReadFile(t *testing.T) {
+	token := github_token
+	owner := github_user
+	repo := github_repo
+	branch := github_ref
+	filename := deploy_config
+
 	transport := &oauth.Transport{
 		Token: &oauth.Token{AccessToken: token},
 	}
 	client := github.NewClient(transport.Client())
-	fmt.Print(client.Repositories.CreateHook("alexkomrakov", "gohub", hook))
 
-	status := new(github.RepoStatus)
-	json.Unmarshal([]byte(`{"state": "success"}`), &status)
-	_, b, _ := client.Repositories.CreateStatus("alexkomrakov", "gohub", "", status)
-	fmt.Println(b)
+	content, _ := getGithubFileContent(client, owner, repo, filename, branch)
+	fmt.Print(string(content))
 
-	listOptions := new(github.ListOptions)
-	json.Unmarshal([]byte(`{"page": 0, "perPage": 5}`), &listOptions)
-	z, c, _ := client.Repositories.ListStatuses("alexkomrakov", "gohub", "", listOptions)
-	fmt.Println(z)
-	fmt.Println(c)
+	conf, _ := readYamlConfig(content)
+	fmt.Println(conf)
 }
 
-func TestReadConfig(t *testing.T)
+func TestExecSshCommand(t *testing.T) {
+	config := &ssh.ClientConfig{
+		User: ssh_user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(ssh_pass),
+		},
+	}
+	client, err := ssh.Dial("tcp", "komrakov-stage.smart-crowd.ru:22", config)
+	if err != nil {
+		fmt.Printf("unable to connect: %s", err)
+	}
+	defer client.Close()
+
+	out, _ := execSshCommand(client, "git")
+	fmt.Print(out.String())
+}

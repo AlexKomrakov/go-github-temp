be9cefbd6b207503c2e753a4ab19ae0316488262
AlexKomrakov <a.komrakov@smart-crowd.ru>
2015-01-25 02:28:50 +0300

    + save

--- main_test.go
+++ main_test.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-//	"code.google.com/p/goauth2/oauth"
-//	"fmt"
-//	"github.com/google/go-github/github"
+	"code.google.com/p/goauth2/oauth"
+	"fmt"
+	"github.com/google/go-github/github"
 //	//	"github.com/stretchr/testify/assert"
 	"testing"
-//	"encoding/json"
+	"encoding/json"
 )
 
 func TestConnect(t *testing.T) {
 //		hook := new(github.Hook)
-//		json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "http://requestb.in/13ymqwv1"}}`), &hook)
-//		fmt.Print(hook)
+//		json.Unmarshal([]byte(`{"name": "web", "active": false, "events": ["pull_request"],	"config": {	"url": "komrakov-stage.smart-crowd.ru:8080/github"}}`), &hook)
+//		fmt.Println(hook)
 //
-//		token := "" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
+//		token := "389924dc1c4981bdd9ffce7bb6de96f7ce18faef" // https://gist.github.com/AlexKomrakov/a55a5867b17eed3057ac
 //		transport := &oauth.Transport{
 //			Token: &oauth.Token{AccessToken: token},
 //		}
 //		client := github.NewClient(transport.Client())
-//		client.Repositories.CreateHook("alexkomrakov", "gohub", hook)
+//		_, _, err := client.Repositories.CreateHook("alexkomrakov", "gohub", hook)
+//		fmt.Println(err)
+
 
 //		status := new(github.RepoStatus)
 //		json.Unmarshal([]byte(`{"state": "success"}`), &status)

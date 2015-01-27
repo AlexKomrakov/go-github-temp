85f63d252cce69156865a22e691c5e968bbffee1
AlexKomrakov <a.komrakov@smart-crowd.ru>
2015-01-27 23:16:31 +0300

    + add public assets (uikit, angularjs) + add new server init file with html template + base realisation for mongo storage methods

---
+++ mongo.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	url = "localhost"
+	collection = "repositories"
+	database = "gohub"
+)
+
+type Repository struct {
+	User       string
+	Repository string
+	Token      string
+}
+
+//TODO defer session.Close()
+func getDb() (*mgo.Database) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	db := session.DB(database)
+	return db
+}
+
+func AddRepository(repo *Repository) {
+	c := getDb().C(collection)
+	err := c.Insert(&repo)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetRepositories() []Repository {
+	c := getDb().C(collection)
+	var repositories []Repository
+	err := c.Find(bson.M{}).All(&repositories)
+	if err != nil {
+		panic(err)
+	}
+	return repositories
+}
+
+func RemoveRepository(repo *Repository) {
+	c := getDb().C(collection)
+	err := c.Remove(repo)
+	if err != nil {
+		panic(err)
+	}
+}

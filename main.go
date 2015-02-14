508c57746de23e25e521824d07ed9aa9323d6268
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-02-14 18:00:09 +0300

    + push event log

--- main.go
+++ main.go
@@ -197,7 +197,7 @@ type PushEventCommit struct {
 	Removed  []string             `json:"removed,omitempty"`
 	Modified []string             `json:"modified,omitempty"`
 }
- 
+
 func GithubHookApi(w http.ResponseWriter, req *http.Request) {
 	body := req.FormValue("payload")
 	event := req.Header["X-Github-Event"][0]

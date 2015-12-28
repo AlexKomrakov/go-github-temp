0ae8b16b4fb2b40d4bb830b27c15e92410543500
AlexKomrakov <a.komrakov@smart-crowd.ru>
2015-12-28 21:15:34 +0300

    + ssh linereader

--- mongo/mongo_test.go
+++ mongo/mongo_test.go
@@ -1,11 +1,12 @@
 package mongo
-//
-//import (
-//	"testing"
-//	"github.com/stretchr/testify/assert"
-//	"fmt"
-//)
-//
+
+import (
+	"testing"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"github.com/mitchellh/go-linereader"
+)
+
 //func TestMongo(t *testing.T) {
 //	repo := Repository{"alexkomrakov", "gohub", "testKey"}
 //	AddRepository(&repo)
@@ -24,3 +25,36 @@ package mongo
 //	repo := branch.GetRepository()
 //	fmt.Print(repo)
 //}
+
+func TestSSH(t *testing.T) {
+	fmt.Println("Hello Starting SSH connect")
+	user     := "root"
+	password := "Zxcfrt6"
+	host     := "188.166.34.149:22"
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+	}
+	client, err := ssh.Dial("tcp", host, config)
+	fmt.Println(err)
+
+	session, err := client.NewSession()
+	fmt.Println(err)
+	defer session.Close()
+
+	reader, _ := session.StdoutPipe()
+	go func() {
+		err := session.Run("traceroute 8.8.8.8")
+		fmt.Println(err)
+	}()
+
+	lr := linereader.New(reader)
+	for line := range lr.Ch {
+		fmt.Println(line)
+	}
+
+	fmt.Println(22)
+}
